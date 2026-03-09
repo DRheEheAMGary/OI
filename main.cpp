3f58dc063981fd2b4bb2d31a50b7c41cb6ee7251
DRheEheAM_GaryHanserV.N.C. <dream20110513Gary@163.com>
2026-03-09 20:22:08 +0800

    new

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*---------------------
+by DRheEheAM (awa)-----
+love hanser forever!---
+---------------------*/
+#include<bits/stdc++.h>
+using namespace std;
+#define intc constexpr int
+#define intl long long
+#define Cios ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+signed main(){
+    return 0;
+}
