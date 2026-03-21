a43e1680eb046182b7671508831080e52d0b3455
DRheEheAM_GaryHanserV.N.C. <dream20110513Gary@163.com>
2026-03-21 21:42:56 +0800

    Completed P7424 , accepted

---
+++ P7424-Accepted.cpp
@@ -0,0 +1,13 @@
+/*---------------------
+by DRheEheAM (awa)-----
+love hanser forever!---
+---------------------*/
+#include<bits/stdc++.h>
+using namespace std;
+#define intc constexpr int
+#define int long long
+#define Cios ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+signed main() {
+    Cios;
+    return 0;
+}
